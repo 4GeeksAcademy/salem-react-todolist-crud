4ef2c4722cb02d4b5f20cfc0d88d90437e50f72f
salemmohdmohd <salim.farouq@gmail.com>
2025-07-25 00:20:04 -0500

    useref controlled input

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,64 @@
-import React from "react";
+import { useState, useRef } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+//  controlled input useRef
 
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+  const [todos, setTodos] = useState([]);
+  const removeTodo = (index) => {
+    const newTodos = todos.filter((predicate, i) => i !== index);
+    setTodos(newTodos);
+    console.log(
+      "removed todo at index: ",
+      index,
+      " ...todos.length: ",
+      newTodos.length
+    );
+  };
+  const todoInputRef = useRef();
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const addTodo = () => {
+    const newTodo = todoInputRef.current.value;
+    if (newTodo) {
+      setTodos([...todos, newTodo]);
+      todoInputRef.current.value = "";
+      console.log("added todos", todos.length);
+    }
+  };
+
+  return (
+    <div className="container-fluid w-50">
+      <h1>todos</h1>
+      <div className=" list-group textinput">
+        <input
+          className="list-group-item textinput"
+          type="text"
+          ref={todoInputRef}
+          placeholder="What needs to be done?"
+          onKeyPress={(e) => {
+            if (e.key === "Enter") {
+              addTodo();
+            }
+          }}
+        />
+        <ul className="list-group shadow-sm ">
+          {todos.map((todo, index) => (
+            <li
+              key={index}
+              className=" textinput list-group-item d-flex justify-content-between align-items-center todo-item"
+            >
+              {index + 1}- {todo}
+              <div className="delete-btn" onClick={() => removeTodo(index)}>
+                ×
+              </div>
+            </li>
+          ))}
+          <div className="list-group-item    ">{todos.length} items left</div>
+          <div className="list-group-item    "></div>
+          <div className="list-group-item    "></div>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
